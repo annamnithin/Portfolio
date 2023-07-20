91deb3bc4cfc0939cb5f373bfd71e07a932d1cd7
annamnithin <nithinsiva71@gmail.com>
2023-07-19 21:48:22 -0400

    changed contact me page and upgraded the dependencies

---
+++ src/app/shared/contact.service.ts
@@ -0,0 +1,49 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {catchError, map, switchMap} from 'rxjs/operators';
+import {of} from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ContactService {
+  private mailApi = 'https://nithin-annam-portfolio.azurewebsites.net/email';
+  private postDBApi = 'https://nithin-annam-portfolio.azurewebsites.net/';
+  private result: string;
+  constructor(private http: HttpClient) {}
+
+  PostMessage(input: any) {
+    this.http.post(this.mailApi, input, {responseType: 'json'})
+      .pipe(
+        switchMap(
+          (response) => {
+            if (response) {
+              // post to another API after a successful first post
+              return this.http.post(this.postDBApi, input, {responseType: 'json'});
+            } else {
+              throw new Error('First request was not successful.');
+            }
+          }
+        ),
+        map((secondResponse) => {
+          if (secondResponse) {
+            // Return a success message for a successful second request
+            return 'Thank you for reaching out! I will get back to you as soon as possible.';
+          } else {
+            throw new Error('Second request was not successful.');
+          }
+        }),
+        catchError((error: any) => {
+          // Return the error message
+          return of(`Request failed: ${error.message}`);
+        })
+      ).subscribe(
+      (message) => {
+        // Store the result to the 'result' variable
+        this.result = message;
+      }
+    );
+  }
+}
